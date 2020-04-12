26131861dc6d02401bf39329657c48fd4af6ac19
Rafaello <rafjar@Rafalek>
2020-04-12 15:32:39 +0200

    Obliczanie fwhr

--- pomoc/punkty_twarzy.py
+++ pomoc/punkty_twarzy.py
@@ -1,48 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 
 data = np.load('dane/pure_landmarks_gender.npy')
 X, y = data[:, :-1], data[:, -1]
+## usunięcie złej twarzyczki
+X = np.delete(X, (8656), axis=0)
+y = np.delete(y, (8656), axis=0)
 
-colors = ['bo', 'go', 'ro', 'co', 'mo', 'yo', 'ko']
+row = 5
 
-fig, (ax1, ax2) = plt.subplots(1,2)
-ax1.set_title('Bez preprocessingu')
-ax2.set_title('Po preprocessingu - preprocessing.Normalizer()')
+xx, yy = X[row, ::2], X[row, 1::2]
+plt.plot(xx, yy, 'bo')
 
-for row, val in enumerate(y):
-    x0, y0 = X[row, 0], X[row, 1]
-    
-    for indx, val in enumerate(X[row, :]):
-        if indx%2 == 0:
-            X[row, indx] -= x0
-        else:
-            X[row, indx] -= y0
-
-X_pre = preprocessing.Normalizer().fit_transform(X)
-for row, val in enumerate(y[8655:8657]):
-    xx_bez, yy_bez = [], []
-    xx_po, yy_po = [], []
-
-    for indx, val in enumerate(X[row, :]):
-        if indx%2 == 0:
-            xx_bez.append(X[row, indx])
-            xx_po.append(X_pre[row, indx])
-        else:
-            yy_bez.append(X[row, indx])
-            yy_po.append(X_pre[row, indx])
-
-    ax1.plot(xx_bez, yy_bez, colors[row%len(colors)])
-    ax2.plot(xx_po, yy_po, colors[row%len(colors)])
-
-    for indx, val in enumerate(xx_bez):
-        if indx == 105 or indx == 72:
-            ax1.annotate(str(indx), (xx_bez[indx], yy_bez[indx]), c='r')
-            ax2.annotate(str(indx), (xx_po[indx], yy_po[indx]), c='r')
-        else:
-            ax1.annotate(str(indx), (xx_bez[indx], yy_bez[indx]))
-            ax2.annotate(str(indx), (xx_po[indx], yy_po[indx]))
-
+for indx in range(len(xx)):
+    plt.annotate(str(indx), (xx[indx], yy[indx]))
 
 plt.show()
